eec021390d402d034d2a6f3bbf2cd52d2f85ba03
rashid234 <rashidkaras23@gmail.com>
2023-02-02 17:38:44 +0530

    fixed errors2

--- Client/src/app/login/login.component.ts
+++ Client/src/app/login/login.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  modal ={ 
-    email:'',
-    password:''
-  }    
+  modal = {
+    email: '',
+    password: ''
+  }
 }
