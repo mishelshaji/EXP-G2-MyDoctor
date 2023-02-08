b181930677f4c9042d22b80925e03d908779604c
ImArjunAsok <arjun14.asok@gmail.com>
2023-02-09 01:17:08 +0530

    Added booking page and changed patient navbar

--- Client/src/app/patient/appointment-booking/appointment-booking.component.ts
+++ Client/src/app/patient/appointment-booking/appointment-booking.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { faCircleArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-appointment-booking',
   templateUrl: './appointment-booking.component.html',
-  styleUrls: ['./appointment-booking.component.css']
+  styleUrls: ['./appointment-booking.component.css'],
 })
 export class AppointmentBookingComponent {
+  faRightArrow = faCircleArrowRight;
 
 }
