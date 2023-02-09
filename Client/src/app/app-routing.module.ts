1f884871f24803e47799950bd70070008bc8754d
fathimasulthanaas <fathimasulthanaas001@gmail.com>
2023-02-09 19:25:55 +0530

    Update app-routing.module.ts

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { OtpPageComponent } from './otp-page/otp-page.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'patient', loadChildren: () => import('./patient/patient.module').then(m => m.PatientModule) },
-  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+  { path: 'otp-page', component:OtpPageComponent}
 ];
 
 @NgModule({
