9dcf2b941b4ec7a9aeb9d89469656546b8f55cdf
ImArjunAsok <arjun14.asok@gmail.com>
2023-02-13 13:54:42 +0530

    Added Doctor Home

--- Client/src/app/doctor/home/home.component.ts
+++ Client/src/app/doctor/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppointmentsService } from 'src/app/services/appointments.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  appointments: any = [];
+  constructor(private AppointmentService: AppointmentsService) {
+
+  }
+  ngOnInit() {
+    this.appointments = this.AppointmentService.getall();
+  }
 
 }
--- Client/src/app/services/appointments.service.ts
+++ Client/src/app/services/appointments.service.ts
@@ -11,20 +11,23 @@ export class AppointmentsService {
       date: '12-02-2023',
       time: '09:30 AM',
       doctor: 'Dr. Arjun',
+      patient: 'Jose Kumar',
       status: true
     },
     {
       id: 2,
       date: '12-02-2023',
       time: '10:30 AM',
-      doctor: 'Dr. vicky',
+      doctor: 'Dr. Arjun',
+      patient: 'Thomas Chacko',
       status: true
     },
     {
       id: 3,
       date: '12-02-2023',
       time: '10:30 AM',
-      doctor: 'Dr. rashid',
+      doctor: 'Dr. Rashid',
+      patient: 'Joshua M.',
       status: true
     }
   ]
