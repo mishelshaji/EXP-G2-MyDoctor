d9149270c2c1ba30f33bdde5c60011be1a26ce2a
rashid234 <rashidkaras23@gmail.com>
2023-02-14 15:52:14 +0530

    Update ApplicationUserService.cs

--- server/MyDoctor/MyDoctor.Service/Services/ApplicationUserService.cs
+++ server/MyDoctor/MyDoctor.Service/Services/ApplicationUserService.cs
@@ -33,7 +33,7 @@ namespace MyDoctor.Service.Services
                 FirstName = dto.FirstName,
                 LastName = dto.LastName,
                 Email = dto.Email,
-                specilization = dto.Specalization,
+                Specilization = dto.Specalization,
                 UserName = Guid.NewGuid().ToString()
             };
             if (dto.Role == "Doctor" || dto.Role == "Patient")
