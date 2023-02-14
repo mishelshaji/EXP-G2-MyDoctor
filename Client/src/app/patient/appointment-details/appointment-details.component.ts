889ef1cf9bebf13bbf4061fc6d20675463e64d3e
fathimasulthanaas <fathimasulthanaas001@gmail.com>
2023-02-14 18:20:10 +0530

    update appointment-detail page

--- Client/src/app/patient/appointment-details/appointment-details.component.ts
+++ Client/src/app/patient/appointment-details/appointment-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppointmentsService } from 'src/app/services/appointments.service';
 
 @Component({
   selector: 'app-appointment-details',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class AppointmentDetailsComponent {
 
+  constructor(private appointmentService: AppointmentsService) {
+
+  }
+  model = {
+    name: '',
+    department: '',
+    email: '',
+    contact: ''
+  }
+  detail = {
+    appointmentno: '',
+    date: '',
+    time: '',
+    status: '',
+    fee: ''
+  }
+
+  medication = {
+    disease: '',
+    description1: '',
+    description2: '',
+    description3: '',
+    description4: '',
+    medicine1: '',
+    medicine2: '',
+    medicine3: '',
+    prefer1: '',
+    prefer2: '',
+    pefer3: ''
+  }
+  ngOnInit() {
+    this.model = this.appointmentService.DoctorDetails;
+    this.detail = this.appointmentService.AppointmentDetails;
+    this.medication = this.appointmentService.Prescription;
+  }
 }
+
