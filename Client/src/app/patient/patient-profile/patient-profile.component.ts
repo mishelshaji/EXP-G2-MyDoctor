84926e7e54a25141c1efb9458866213f7fe780a5
josephvicky02 <josephvicky02@gmail.com>
2023-02-14 18:06:19 +0530

    Update patient-profile.component.ts

--- Client/src/app/patient/patient-profile/patient-profile.component.ts
+++ Client/src/app/patient/patient-profile/patient-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PatientService } from 'src/app/services/patient.service';
 
 @Component({
   selector: 'app-patient-profile',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./patient-profile.component.css']
 })
 export class PatientProfileComponent {
+  model = {
+    firstName: '',
+    lastName: '',
+    dob: '',
+    gender: '',
+    email: '',
+    number: ''
+  }
 
+  constructor(private patientServices: PatientService) {
+    
+  }
+
+  saveData(){
+    this.patientServices.updateProfile(this.model);
+  }
 }
+
