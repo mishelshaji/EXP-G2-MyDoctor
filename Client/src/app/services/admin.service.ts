ae420f9dd12a67c3c804c25b58629dfaf9804e7b
ImArjunAsok <arjun14.asok@gmail.com>
2023-02-19 23:33:45 +0530

    Update admin.service.ts

--- Client/src/app/services/admin.service.ts
+++ Client/src/app/services/admin.service.ts
@@ -7,32 +7,27 @@ import { Injectable } from '@angular/core';
 export class AdminService {
   url = 'https://localhost:7238/api/Admin/Dashboard'
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient) {
 
   }
 
-  getPatientsCount()
-  {
+  getPatientsCount() {
     return this.http.get(this.url + '/PatientsCount');
   }
 
-  getDoctorsCount()
-  {
+  getDoctorsCount() {
     return this.http.get(this.url + '/DoctorsCount');
   }
 
-  getUpcomingBookingsCount()
-  {
+  getUpcomingBookingsCount() {
     return this.http.get(this.url + '/UpcomingBookingsCount');
   }
 
-  getCompletedBookingsCount()
-  {
+  getCompletedBookingsCount() {
     return this.http.get(this.url + '/CompletedBookingsCount');
   }
 
-  getRegistrationApprovalsCount()
-  {
+  getRegistrationApprovalsCount() {
     return this.http.get(this.url + '/RegistrationApprovalsCount');
   }
 }
